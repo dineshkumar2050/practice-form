4f2b87e7991933cb47dcfbea6c7bf200f018b5b0
dineshkumar2050 <dineshkumarjhangra@gmail.com>
2021-03-24 22:25:14 +0530

    first commit

---
+++ src/WelcomeUser.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function WelcomeUser(props) {
+    return (
+        <div>
+            Welcome user
+        </div>
+    )
+}
+
+WelcomeUser.propTypes = {
+
+}
+
+export default WelcomeUser
+
